2dea0dbb3b3687c3a57baa19588d83b0e7d7e3fc
Nicholas Walton <nicholas.walton@gmail.com>
2013-10-11 19:02:23 -0700

    Roughly hack in OpenBSD netcat, because I need the -U (AF_UNIX) feature.

--- package/netcat/netcat.mk
+++ package/netcat/netcat.mk
@@ -4,8 +4,25 @@
 #
 #############################################################
 
-NETCAT_VERSION = 0.7.1
-NETCAT_SOURCE = netcat-$(NETCAT_VERSION).tar.gz
-NETCAT_SITE = http://downloads.sourceforge.net/project/netcat/netcat/$(NETCAT_VERSION)
+NETCAT_VERSION = 5
+# NETCAT_SOURCE = netcat-$(NETCAT_VERSION).tar.gz
+NETCAT_SITE = http://openbsd-netcat.googlecode.com/svn/trunk/
+NETCAT_SITE_METHOD=svn
 
-$(eval $(autotools-package))
+define NETCAT_BUILD_CMDS
+	$(MAKE1) CC="$(TARGET_CC)" LD="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D)
+endef
+
+define NETCAT_INSTALL_TARGET_CMDS
+	cp $(@D)/nc $(TARGET_DIR)/usr/bin
+endef
+
+define NETCAT_CLEAN_CMDS
+	-$(MAKE1) CC="$(TARGET_CC)" LD="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D) clean
+endef
+
+define NETCAT_UNINSTALL_TARGET_CMDS
+	rm $(TARGET_DIR)/usr/bin/nc
+endef
+
+$(eval $(generic-package))
