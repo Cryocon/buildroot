b300b72f7b84cc31e4378a1cd99854707d2568d8
Nicholas Walton <nicholas.walton@gmail.com>
2013-10-11 19:01:12 -0700

    Add support for easy_install of arbitrary Python modules.

---
+++ package/python-additional/python-additional.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# python-additional
+#
+#############################################################
+
+PYTHON_ADDITIONAL_DEPENDENCIES = python
+PYTHON_ADDITIONAL_VERSION =
+PYTHON_ADDITIONAL_SOURCE  =
+PYTHON_ADDITIONAL_SITE    =
+
+#define PYTHON_ADDITIONAL_BUILD_CMDS
+#	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
+#endef
+
+define PYTHON_ADDITIONAL_INSTALL_TARGET_CMDS
+	(export PYTHONPATH=$(TARGET_DIR)/usr/lib/python2.7/site-packages; cd $(@D); $(HOST_DIR)/usr/bin/easy_install --prefix=$(TARGET_DIR)/usr --always-unzip --build-directory $(BUILD_DIR)/python-additional --local-snapshots-ok $(call qstrip,$(BR2_PACKAGE_PYTHON_ADDITIONAL)))
+endef
+
+$(eval $(generic-package))
