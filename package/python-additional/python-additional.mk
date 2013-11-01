7075aaf228e6a128e9e73c0c303a4c3277149ae0
Nicholas Walton <nicholas.walton@gmail.com>
2013-11-01 13:28:54 -0700

    Fix python-additional to build automatically.

--- package/python-additional/python-additional.mk
+++ package/python-additional/python-additional.mk
@@ -14,7 +14,7 @@ PYTHON_ADDITIONAL_SITE    =
 #endef
 
 define PYTHON_ADDITIONAL_INSTALL_TARGET_CMDS
-	(export PYTHONPATH=$(TARGET_DIR)/usr/lib/python2.7/site-packages; cd $(@D); $(HOST_DIR)/usr/bin/easy_install --prefix=$(TARGET_DIR)/usr --always-unzip --build-directory $(BUILD_DIR)/python-additional --local-snapshots-ok $(call qstrip,$(BR2_PACKAGE_PYTHON_ADDITIONAL)))
+	(export PYTHONPATH=$(TARGET_DIR)/usr/lib/python2.7/site-packages; cd $(@D); $(HOST_DIR)/usr/bin/easy_install --prefix=$(TARGET_DIR)/usr --always-unzip --build-directory $(BUILD_DIR)/python-additional --local-snapshots-ok $(call qstrip,$(BR2_PACKAGE_PYTHON_ADDITIONAL_PACKAGES)))
 endef
 
 $(eval $(generic-package))
