ef44f0972b28eb4a964b6e9259df93b830a25060
Nicholas Walton <nicholas.walton@gmail.com>
2014-01-14 18:35:53 -0800

    Include cryocond files in images (for NAND).

---
+++ board/cryocon/post-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DEST=$1
+# Copy all files
+cp -rL ~/cryocond/root/* $DEST &&
+# Copy cryocond executable
+cp -L ~/cryocond/M18I/ARM/cryocond $DEST/var &&
+# Install web pages
+# Install updater
+cp -rL ~/cryocond/new_web/* $DEST/var &&
+# Copy splash screen executable
+(cp -L ~/cryocond/splash/ARM/splash $DEST/opt || true) &&
+# Install user's public key
+cat ~/.ssh/id_rsa.pub | tee -a $DEST/root/.ssh/authorized_keys
