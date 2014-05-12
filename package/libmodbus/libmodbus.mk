ead25a43a6942b6c1d3bd36a05b917b5828b3ef5
Nicholas Walton <nicholas.walton@gmail.com>
2014-05-12 15:45:44 -0700

    libmodbus: bump version

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -3,8 +3,8 @@
 # libmodbus
 #
 #############################################################
-LIBMODBUS_VERSION = 3.0.3
-LIBMODBUS_SITE = http://github.com/downloads/stephane/libmodbus
+LIBMODBUS_VERSION = 3.0.6
+LIBMODBUS_SITE = http://libmodbus.org/releases/
 LIBMODBUS_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
