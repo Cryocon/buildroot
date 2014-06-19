c70c8f2a8093e726efc99a398e0132af372c2607
Nicholas Walton <nicholas.walton@gmail.com>
2014-06-19 11:52:40 -0700

    Correct Python path for gunicorn start script

--- board/cryocon/post-build.sh
+++ board/cryocon/post-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DEST=$1
+# Update Python path
+(echo "#!/usr/bin/python" && tail -n +2 $DEST/usr/bin/gunicorn) > $DEST/usr/bin/gunicorn.new &&
+chmod --reference=$DEST/usr/bin/gunicorn $DEST/usr/bin/gunicorn.new &&
+mv $DEST/usr/bin/gunicorn.new $DEST/usr/bin/gunicorn &&
 # Copy all files
 (cp -rvL ~/cryocond/root/* $DEST || true) &&
 # Install web pages
