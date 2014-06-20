a4b08a7ffedc3c8cd0734a048f6f671d220e5e7c
Nicholas Walton <nicholas.walton@gmail.com>
2014-06-20 12:02:23 -0700

    python-ipython: Add missing Notebook dependencies.
    IPython Notebook needs Jinja2 and Tornado at build time, and pyzmq at
    run time.

--- package/python-ipython/python-ipython.mk
+++ package/python-ipython/python-ipython.mk
@@ -2,11 +2,14 @@
 #
 # python-ipython
 #
+# Note that python-ipython will build Notebook even without pyzmq, and pyzmq
+# detection will fail.
 ################################################################################
 
 PYTHON_IPYTHON_VERSION = 1.1.1-dev
 PYTHON_IPYTHON_SITE = ~/src/ipython
 PYTHON_IPYTHON_SITE_METHOD=local
 PYTHON_IPYTHON_SETUP_TYPE = distutils
+PYTHON_IPYTHON_DEPENDENCIES = python-pyzmq python-tornado python-additional
 
 $(eval $(python-package))
