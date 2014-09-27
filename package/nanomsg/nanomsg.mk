27dd1997b270c0e9aa9191839c09b3f9e9af3adc
Nicholas Walton <nicholas.walton@gmail.com>
2014-09-26 17:58:09 -0700

    Install nanomsg headers

--- package/nanomsg/nanomsg.mk
+++ package/nanomsg/nanomsg.mk
@@ -8,5 +8,6 @@ NANOMSG_VERSION = 0.4-beta
 NANOMSG_SITE = http://download.nanomsg.org/
 NANOMSG_LICENSE = MIT/X11
 NANOMSG_LICENSE_FILES = COPYING
+NANOMSG_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
