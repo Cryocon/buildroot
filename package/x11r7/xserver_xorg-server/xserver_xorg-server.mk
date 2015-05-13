7090f856e29738ecfcaf4614fe7f8f76adeba8de
Nicholas Walton <nicholas.walton@gmail.com>
2015-05-13 14:27:28 -0700

    xserver_xorg-server: Explicitly disable DRI in CONF_OPTS

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -204,15 +204,15 @@ endif
 ifeq ($(BR2_PACKAGE_XPROTO_DRI3PROTO),y)
 XSERVER_XORG_SERVER_DEPENDENCIES += xlib_libxshmfence xproto_dri3proto
 XSERVER_XORG_SERVER_CONF_OPTS += --enable-dri3
+else
+XSERVER_XORG_SERVER_CONF_OPTS += --disable-dri3
+endif
 ifeq ($(BR2_PACKAGE_LIBEPOXY),y)
 XSERVER_XORG_SERVER_DEPENDENCIES += libepoxy
 XSERVER_XORG_SERVER_CONF_OPTS += --enable-glamor
 endif
 else
 XSERVER_XORG_SERVER_CONF_OPTS += --disable-dri3 --disable-glamor
-endif
-else
-XSERVER_XORG_SERVER_CONF_OPTS += --disable-dri2 --disable-dri3 --disable-glamor
 endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXSCRNSAVER),y)
