f4ab2ff1c1ad72614ee008d4f2ba5b9833d93ac1
Nicholas Walton <nicholas.walton@gmail.com>
2015-07-08 16:41:44 -0700

    libflek: New package

---
+++ package/flek/flek.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# FLEK
+#
+################################################################################
+
+FLEK_VERSION = master
+FLEK_SOURCE = flek-$(FLEK_VERSION)-source.tar.gz
+FLEK_SITE = ~/src/flek
+FLEK_SITE_METHOD = git
+FLEK_INSTALL_STAGING = YES
+FLEK_DEPENDENCIES = fltk
+#FLEK_CONFIG_SCRIPTS = flek-config
+FLEK_LICENSE = LGPLv2
+FLEK_LICENSE_FILES = README
+
+FLEK_CONF_OPTS = --disable-gl --disable-jpeg --with-fltk=$(TARGET_DIR)/usr 
+FLEK_CONF_OPTS += --exec-prefix=$(STAGING_DIR)/usr --prefix=$(STAGING_DIR)/usr
+
+$(eval $(autotools-package))
