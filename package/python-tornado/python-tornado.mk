49d7c9938b81d869f2dbb2cb23ed63d7c62eaed4
Nicholas Walton <nicholas.walton@gmail.com>
2016-03-30 14:46:35 -0700

    python-tornado: remove tests from target
    Saves about 1M of space

--- package/python-tornado/python-tornado.mk
+++ package/python-tornado/python-tornado.mk
@@ -10,4 +10,11 @@ PYTHON_TORNADO_SITE = https://pypi.python.org/packages/source/t/tornado
 PYTHON_TORNADO_LICENSE = Apache-2.0
 PYTHON_TORNADO_SETUP_TYPE = setuptools
 
+define PYTHON_TORNADO_REMOVE_TEST
+        rm -rf $(TARGET_DIR)/usr/lib/python*/site-packages/tornado/test
+endef
+
+PYTHON_TORNADO_POST_INSTALL_TARGET_HOOKS += PYTHON_TORNADO_REMOVE_TEST
+
+
 $(eval $(python-package))
