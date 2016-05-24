53504d111ed7ff9886284ec3a4bda9835a480a57
Nicholas Walton <nicholas.walton@gmail.com>
2016-05-24 15:21:23 -0700

    cryocon: m54: Preserve timestamps when creating fpga0.bit.installed file

--- board/cryocon/m54/rootfs_overlay/usr/bin/update-fpga0.sh
+++ board/cryocon/m54/rootfs_overlay/usr/bin/update-fpga0.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 diff /boot/fpga0.bit /boot/fpga0.bit.installed ||
-( /usr/bin/image-fpga.sh && cp /boot/fpga0.bit /boot/fpga0.bit.installed )
+( /usr/bin/image-fpga.sh && cp -a /boot/fpga0.bit /boot/fpga0.bit.installed )
