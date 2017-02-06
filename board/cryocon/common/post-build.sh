06ffc4bd64cd3cd02620761cc093eebd904b0f94
Nicholas Walton <nicholas.walton@gmail.com>
2017-02-06 14:09:35 -0800

    cryocon: Delete /var/spool and /var/cache symlinks in post-build script

--- board/cryocon/common/post-build.sh
+++ board/cryocon/common/post-build.sh
@@ -4,6 +4,8 @@ DEST=$1
 (echo "#!/usr/bin/python" && tail -n +2 $DEST/usr/bin/gunicorn) > $DEST/usr/bin/gunicorn.new &&
 chmod --reference=$DEST/usr/bin/gunicorn $DEST/usr/bin/gunicorn.new &&
 mv $DEST/usr/bin/gunicorn.new $DEST/usr/bin/gunicorn &&
+(rm -f $DEST/var/spool $DEST/var/cache || true) &&
+mkdir -p $DEST/var/spool $DEST/var/cache &&
 # Copy all files
 (cp -rvL ../cryocond/root/* $DEST || true) &&
 # Install web pages
