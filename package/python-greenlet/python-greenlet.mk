6f870c8888df417d93675b26658af4ff0bc1c4b7
Nicholas Walton <nicholas.walton@gmail.com>
2017-02-14 14:02:06 -0800

    python-greenlet: Regenerate using scanpypi

--- package/python-greenlet/python-greenlet.mk
+++ package/python-greenlet/python-greenlet.mk
@@ -4,15 +4,11 @@
 #
 ################################################################################
 
-PYTHON_GREENLET_VERSION = 0.4.9
-PYTHON_GREENLET_SOURCE  = greenlet-$(PYTHON_GREENLET_VERSION).zip
-PYTHON_GREENLET_SITE    = https://pypi.python.org/packages/source/g/greenlet
 PYTHON_GREENLET_SETUP_TYPE = distutils
-
-define PYTHON_GREENLET_EXTRACT_CMDS
-        unzip -d $(@D) $(DL_DIR)/$(PYTHON_GREENLET_SOURCE)
-        mv $(@D)/*/* $(@D)
-endef
-
+PYTHON_GREENLET_VERSION = 0.4.12
+PYTHON_GREENLET_SOURCE = greenlet-$(PYTHON_GREENLET_VERSION).tar.gz
+PYTHON_GREENLET_SITE = https://pypi.python.org/packages/be/76/82af375d98724054b7e273b5d9369346937324f9bcc20980b45b068ef0b0
+PYTHON_GREENLET_LICENSE = MIT
+PYTHON_GREENLET_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
