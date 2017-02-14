94fceab5272b1075367e4f146f3ca1c6ed084b58
Nicholas Walton <nicholas.walton@gmail.com>
2017-02-14 14:03:13 -0800

    ipython: Use supermodule path for local site instead of user path

--- package/python-ipython/python-ipython.mk
+++ package/python-ipython/python-ipython.mk
@@ -7,7 +7,7 @@
 ################################################################################
 
 PYTHON_IPYTHON_VERSION = 1.1.1-dev
-PYTHON_IPYTHON_SITE = ~/src/ipython
+PYTHON_IPYTHON_SITE = ../src/ipython
 PYTHON_IPYTHON_SITE_METHOD=local
 PYTHON_IPYTHON_LICENSE = BSD-3c
 PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst
