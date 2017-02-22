5e5237d3bd6b77adcc1f89c4848fb5b2fe792ffb
Nicholas Walton <nicholas.walton@gmail.com>
2017-02-22 13:16:35 -0800

    python-gevent: Bump to 1.2.1

--- package/python-gevent/python-gevent.mk
+++ package/python-gevent/python-gevent.mk
@@ -4,16 +4,18 @@
 #
 ################################################################################
 
-PYTHON_GEVENT_VERSION = 1.0.1
-PYTHON_GEVENT_SOURCE  = gevent-$(PYTHON_GEVENT_VERSION).tar.gz
-PYTHON_GEVENT_SITE    = https://pypi.python.org/packages/source/g/gevent
-PYTHON_GEVENT_DEPENDENCIES = libev python-greenlet
-PYTHON_GEVENT_SETUP_TYPE = distutils
+PYTHON_GEVENT_VERSION = 1.2.1
+PYTHON_GEVENT_SOURCE = gevent-$(PYTHON_GEVENT_VERSION).tar.gz
+PYTHON_GEVENT_SITE = https://pypi.python.org/packages/54/dd/17dc7e899ac7c1de2d19b367b29d90fdb4cfe83bda8c2581464906c9399d
+PYTHON_GEVENT_SETUP_TYPE = setuptools
+PYTHON_GEVENT_LICENSE = MIT
+PYTHON_GEVENT_LICENSE_FILES = LICENSE deps/libev/LICENSE
 
-define PYTHON_GEVENT_EXTRACT_CMDS
-	tar xzf $(DL_DIR)/$(PYTHON_GEVENT_SOURCE) -C $(@D) --strip-components=1
-	rm -rf $(@D)/libev
-	ln -s ../libev-4.11 $(@D)/libev
+ifneq ($(BR2_PACKAGE_PYTHON3),y)
+define PYTHON_GEVENT_REMOVE_SOCKET3
+        $(RM) $(@D)/src/gevent/_socket3.py
 endef
+PYTHON_GEVENT_PRE_BUILD_HOOKS += PYTHON_GEVENT_REMOVE_SOCKET3
+endif
 
 $(eval $(python-package))
