d45adab431084ae518d9854b1203f437f22bab11
Guy Covert <guy@cryocon.com>
2017-07-18 13:04:44 -0700

    python-spitfire: Change site/method from local to Github

--- package/python-spitfire/python-spitfire.mk
+++ package/python-spitfire/python-spitfire.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-PYTHON_SPITFIRE_VERSION = master
-PYTHON_SPITFIRE_SITE    = ../src/spitfire
-PYTHON_SPITFIRE_SITE_METHOD = local
+PYTHON_SPITFIRE_VERSION = 4e974e996bb66bf13440a99c23eeb0183c64b235
+PYTHON_SPITFIRE_SITE    = https://github.com/youtube/spitfire.git
+PYTHON_SPITFIRE_SITE_METHOD = git
 PYTHON_SPITFIRE_DEPENDENCIES = python
 PYTHON_SPITFIRE_LICENSE = BSD-3c
 PYTHON_SPITFIRE_LICENSE_FILES = LICENSE
